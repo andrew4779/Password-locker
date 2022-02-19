e8419829504455fcdccf1b7f333da6801d82410c
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-02-20 00:21:41 +0300

    new commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+def __init__(self, user_name, password):
+  '''
+  __init__ method that helps us defeine properties of our objejcts
+
+  Args:
+      user_name: New account user name.
+      password: new account password.
+  '''
